ce36be364e13432eb7c943446419dbc2b3665ce2
fani-01 <chintaphanithkumar@gmail.com>
2025-06-26 17:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import tensorflow as tf
 
 app = Flask(__name__)
-model = tf.keras.models.load_model('healthy_vs_rotten.h5.ipynb')
+model = tf.keras.models.load_model('healthy_vs_rotten.h5')
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
